3d06a38fce272ba59121ef6a6e7537be9e9b0303
Jay A <justynadusei@gmail.com>
2020-04-02 09:54:42 +0000

    Build array to display country flags

--- Swift100Milestone1/ViewController.swift
+++ Swift100Milestone1/ViewController.swift
@@ -9,10 +9,14 @@
 import UIKit
 
 class ViewController: UITableViewController {
+    
+    var countires = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // List of available countries
+        countires += ["Estonia", "France", "Germany", "Ireland", "Italy", "Monaco", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"]
     }
 
 
