86c32efc454e4c3834bfb48c54f77e1f6b424ddb
Jay A <justynadusei@gmail.com>
2020-04-02 11:02:24 +0000

    Displayed list of flags

--- Swift100Milestone1/ViewController.swift
+++ Swift100Milestone1/ViewController.swift
@@ -10,15 +10,51 @@ import UIKit
 
 class ViewController: UITableViewController {
     
-    var countires = [String]()
+    var countries = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // List of available countries
-        countires += ["Estonia", "France", "Germany", "Ireland", "Italy", "Monaco", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"]
+        countries += ["Estonia", "France", "Germany", "Ireland", "Italy", "Monaco", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"]
+        
+        // Navbar title
+        title = "SELECT A COUNTRY"
+        
+        // Making the title big & dynamic
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        // Loading images from disk
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let flags = try! fm.contentsOfDirectory(atPath: path)
+        
+        for flag in flags {
+            
+            if flag.hasSuffix("png") {
+            
+            countries.append(flag)
+            
+            }
+        }
+    
+        print(countries)
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+
+        // Set number of rows
+        countries.count
     }
 
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Flag", for: indexPath)
+        cell.textLabel?.text = countries[indexPath.row]
+        return cell
+
+    }
 
 }
 
